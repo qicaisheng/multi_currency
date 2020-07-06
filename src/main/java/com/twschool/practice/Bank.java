19edf0bdd8200993114c44878ce74d12f192e896
qicaisheng <qicaisheng@qq.com>
2020-07-06 15:50:00 +0800

    should_return_5_USD_when_transform_with_USD_given_10_CHF

--- src/main/java/com/twschool/practice/Bank.java
+++ src/main/java/com/twschool/practice/Bank.java
@@ -5,6 +5,11 @@ public class Bank {
         if (currency.equals(money.getCurrency())) {
             return money;
         }
+        if (currency.equals("USD")) {
+            if (money.getCurrency().equals("CHF")) {
+                return new Money(money.getValue() / 2, "USD");
+            }
+        }
         return null;
     }
 }
--- src/test/java/com/twschool/practice/BankTest.java
+++ src/test/java/com/twschool/practice/BankTest.java
@@ -14,4 +14,14 @@ public class BankTest {
         Assert.assertEquals(5, transformedMoney.getValue());
         Assert.assertEquals("USD", transformedMoney.getCurrency());
     }
+
+    @Test
+    public void should_return_5_USD_when_transform_with_USD_given_10_CHF() {
+        Money money = new Money(10, "CHF");
+        Bank bank = new Bank();
+
+        Money transformedMoney = bank.transformWith(money, "USD");
+        Assert.assertEquals(5, transformedMoney.getValue());
+        Assert.assertEquals("USD", transformedMoney.getCurrency());
+    }
 }
